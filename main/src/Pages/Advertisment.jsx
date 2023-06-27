df47ec3042e0c3110d8aef7c2405f7714da7d199
Naveed-Shaikh6 <naveedshaikh8859@gmail.com>
2023-06-27 12:35:38 +0530

    All Updated Files

--- main/src/Pages/Advertisment.jsx
+++ main/src/Pages/Advertisment.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ad1 from '../Images for ads/ad1.png'
 function Advertisment() {
-  return (
-    <>
-        <div className='flex justify-center mt-4 mb-3'>
-        <h1 className='text-xl font-bold pl-12'>Ads:</h1>
-        <a href='https://play.google.com/store/search?q=google+ads&c=apps&hl=en&gl=US'>
-        <img className='cursor-pointer' src={ad1} />
-        </a>
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className='flex justify-center mt-4 mb-3'>
+                <h1 className='text-xl font-bold pl-12'>Ads:</h1>
+                <a href='https://play.google.com/store/search?q=google+ads&c=apps&hl=en&gl=US'>
+                    <img className='cursor-pointer' src={ad1} />
+                </a>
+            </div>
+        </>
+    )
 }
 
 export default Advertisment
